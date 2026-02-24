ffdfddcba698357c4fc7a6fa76df0228365a6414
Victor Souza <souzamvictor@gmail.com>
2026-02-24 01:23:22 -0300

    styling our texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-export default function App() {
+import Text from "./assets/components/text";
 
-  return(
-   <div className="text-green-dark text-2xl">Hello, World!</div>
-  )
+export default function App() {
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">Hello, World!</Text>
+
+      <Text className="text-green-base">Hello, World!</Text>
+
+      <Text variant="body-md-bold">Hello, World!</Text>
+
+      <Text>Teste de texto</Text>
+    </div>
+  );
 }
