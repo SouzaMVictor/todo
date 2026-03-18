bc024ce4856ada0046f65725d9d92c231ce7c41f
Victor Souza <souzamvictor@gmail.com>
2026-03-18 20:11:14 -0300

    updating task

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -14,6 +14,7 @@ import InputText from "../components/input-text";
 
 import { Task, TaskState } from "../models/task";
 import { cx } from "class-variance-authority";
+import useTask from "../hooks/use-task";
 
 interface TaskItemProps {
   task: Task;
@@ -24,7 +25,8 @@ export default function TaskItem({ task }: TaskItemProps) {
     task?.state === TaskState.Creating,
   );
 
-  const [taskTitle, setTaskTitle] = React.useState("");
+  const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+  const {updateTask} = useTask();
 
   function handleEditTask() {
     setIsEditing(true);
@@ -41,6 +43,7 @@ export default function TaskItem({ task }: TaskItemProps) {
   function handleSaveTask(e: React.ChangeEvent<HTMLFormElement>){
     e.preventDefault();
     console.log({id: task.id, title: taskTitle})
+    updateTask(task.id, {title: taskTitle})
     setIsEditing(false);
   }
 
@@ -73,6 +76,7 @@ export default function TaskItem({ task }: TaskItemProps) {
         <>
           <form onSubmit={handleSaveTask} className="flex items-center gap-4">
             <InputText
+              value={taskTitle}
               className="flex-1"
               onChange={handleChangeTaskTitle}
               required
